3c254dadd1e4913367b1800033c63b22e9e37ba1
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2024-09-19 23:53:12 -0400

    Improve experiment logging

--- experiment-artificial/artificial.py
+++ experiment-artificial/artificial.py
@@ -7,6 +7,7 @@ from random import choices
 from string import ascii_lowercase
 from os import makedirs
 from os.path import join
+from tqdm import tqdm, trange
 
 llvm = '/usr/src/app/llvm-abom/build/bin'
 
@@ -17,8 +18,8 @@ def random_abom_hash() -> str:
     return abom_hash(randint(0, 2**36).to_bytes(5, 'little'))
 
 def main(directory: str, dependency_count: int, collision_attempts: int):
-    print(f'Generating C solution in {directory} with {dependency_count} dependencies...', end=' ')
     # Generate artificial C solution
+    print(f'Generating C solution in {directory} with {dependency_count} dependencies...', end=' ', flush=True)
     try:
         makedirs(join(directory, 'src'))
         makedirs(join(directory, 'include'))
@@ -41,32 +42,34 @@ def main(directory: str, dependency_count: int, collision_attempts: int):
         for dep in deps:
             f.write(f'#include "{dep}.h"\n')
         f.write('\nint main() {\n   return 0;\n}\n')
-    print('Done')
+    print('Done', flush=True)
     # Compile artificial C solution
-    print(f'Compiling generated C solution with ABOM...', end=' ')
+    print(f'Compiling generated C solution with ABOM...', end=' ', flush=True)
     binary = f'{directory}/bin/abom{dependency_count}'
     run(f"{llvm}/clang "
         f"-fabom "
-        f"-fuse-ld={llvm}/lld "
+        f"-fuse-ld={llvm}/ld.lld "
         f"-I {directory}/include "
         f"-o {binary} "
         f"{directory}/src/abom{dependency_count}.c "
         f"{' '.join([f'{directory}/src/{dep}.c' for dep in deps])}",
         shell=True)
-    print('Done')
-    print('Generated ABOM details:')
+    print('Done', flush=True)
+    print('Generated ABOM details:', flush=True)
     result = run(f'{llvm}/llvm-readobj --abom {binary}', capture_output=True, shell=True)
+    print(result.stdout.decode(), flush=True)
     # Calculate true set of hahes
-    print('Checking ABOM for true set of hashes...', end=' ')
+    print('Checking ABOM for true set of hashes...', flush=True)
     true_hashes = set()
     for dep in deps:
         c_hash = abom_hash(open(f'{directory}/src/{dep}.c', 'rb').read())
         h_hash = abom_hash(open(f'{directory}/include/{dep}.h', 'rb').read())
-        true_hashes.add(hash)
+        true_hashes.add(c_hash)
+        true_hashes.add(h_hash)
     # Validate ABOM contains true hashes
     true_present = 0
     true_absent = 0
-    for hash in true_hashes:
+    for hash in tqdm(true_hashes):
         result = run(f'{llvm}/llvm-abom-check {binary} {hash}',
                       capture_output=True, shell=True)
         if result.stdout.startswith(b'Present'):
@@ -76,16 +79,16 @@ def main(directory: str, dependency_count: int, collision_attempts: int):
         else:
             print(f'Error: {hash}')
             exit(1)
-    print('Done')
+    print('Done', flush=True)
     print(f'===\nTrue Hashes Present: {true_present}\n'
           f'True Hahes Absent: {true_absent}\n'
-          f'True Hashes Success Rate: {true_present / len(true_hashes):.0%}\n===')
+          f'True Hashes Success Rate: {true_present / len(true_hashes):.4%}\n===', flush=True)
     # Test for false positives
-    print(f'Generating {collision_attempts} random hashes to test for false positives...', end=' ')
+    print(f'Generating {collision_attempts} random hashes to test for false positives...', flush=True)
     false_hashes = set()
     false_present = 0
     false_absent = 0
-    for i in range(collision_attempts):
+    for i in trange(collision_attempts):
         while True:
             hash = random_abom_hash()
             if hash not in true_hashes and hash not in false_hashes:
@@ -103,7 +106,7 @@ def main(directory: str, dependency_count: int, collision_attempts: int):
     print('Done')
     print(f'===\nFalse Hashes Present: {false_present}\n'
           f'False Hashes Absent: {false_absent}\n'
-          f'False Hashes Success Rate: {false_absent / len(false_hashes):.0%}\n===')
+          f'False Positive Rate: {false_present / len(false_hashes):.4%}\n===')
     print('Experiment Complete.')
 
 if __name__ == '__main__':
